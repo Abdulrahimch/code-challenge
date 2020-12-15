c1adec478e3b9aca4343b85ba4649b1b31ba73c6
cisco <abooodch101@gmail.com>
2020-12-15 22:52:05 +0300

    Customizing admin UI to add users and customers

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,7 +7,7 @@ class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'room_number')}),
+        (None, {'fields': ('email', 'password')}),
         (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
@@ -31,3 +31,4 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Customer)
--- app/core/models.py
+++ app/core/models.py
@@ -17,6 +17,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password):
         user = self.create_user(email, password)
         user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
